154b81caa28264c180868246fb2776c69d3e0fa2
ducanh1997 <ducanh8098@gmail.com>
2023-08-04 18:11:39 +0700

    update Cat file

---
+++ Mouse.js
@@ -0,0 +1,11 @@
+function Mouse(eyes, tail, ears, foot) {
+    this.eyes = eyes;
+    this.tail = tail;
+    this.ears = ears;
+    this.foot = foot;
+}
+
+const jerry = new Mouse("black", "short", "long", "strong");
+
+
+module.export = Cat;
